6de4f209eee1f388a9c641ad39dc64b0f2f03ed2
OmarFaccuseh <omar_fs.07@hotmail.com>
2024-02-10 16:17:24 -0600

    disable ready

--- inventario/apps.py
+++ inventario/apps.py
@@ -3,7 +3,8 @@ from django.apps import AppConfig
 
 #@staticmethod
 def getOrders():
-    
+    return "NOT ABLE"
+    """
     import requests
     import json
     from .models import Order, Tokens
@@ -60,7 +61,7 @@ def getOrders():
             new_order_obj = Order.objects.create(**data)
             new_order_obj.save()
     return "OK"
-    
+    """
 
 
 class InventarioConfig(AppConfig):
@@ -70,6 +71,8 @@ class InventarioConfig(AppConfig):
     # FIXME: if new ddbb, should comment this function, because would not have make querys here
     
     def ready(self):
+        pass
+        """
         import requests
         from .models import Tokens
         from datetime import datetime, timezone, timedelta
@@ -111,4 +114,5 @@ class InventarioConfig(AppConfig):
                 print(e)
 
         getOrders()
+    """
     
