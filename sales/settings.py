a52e42111af24fcedd44f1c72c68f08dc918c519
OmarFaccuseh <omar_fs.07@hotmail.com>
2024-02-10 17:49:41 -0600

    fix basedir

--- sales/settings.py
+++ sales/settings.py
@@ -13,12 +13,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dotenv
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASE_URL= os.getenv('DATABASE_URL')
 
