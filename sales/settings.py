86b09d9af87359a92631998e2cedd990362ba317
OmarFaccuseh <omar_fs.07@hotmail.com>
2024-07-04 15:09:04 -0600

    add host allowed

--- sales/settings.py
+++ sales/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 CORS_ORIGIN_ALLOW_ALL = DEBUG
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'web-production-03ae.up.railway.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'web-production-03ae.up.railway.app', 'aws-0-us-west-1.pooler.supabase.com', django-sales.onrender.com]
 
 
 # Application definition
